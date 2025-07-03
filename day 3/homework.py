5dbedcfeb9b09eac02f6ca1ac4f49f64ad9fae67
archiscoobert <archidreamer13@gmail.com>
2025-07-03 17:48:48 +0400

    first commit

---
+++ day 3/homework.py
@@ -0,0 +1,15 @@
+#mom
+
+momename = ("Qristine")
+
+#dad
+
+dadname = ("Sandro")
+
+#me
+
+mename = ("Archili")
+
+print(momename)
+print(dadname)
+print(mename)
---
+++ day2/lesson2.py
@@ -0,0 +1,10 @@
+print("Archil chikovani")
+
+name = ("archil")
+lastname = ("chikovani")
+age = 10
+
+
+print(name)
+print(lastname)
+print(age)
